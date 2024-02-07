8d9d1fa99e5cf432987a5fb731bacea03747bd1b
RoReeceDev <roreecedev@gmail.com>
2024-02-07 11:27:57 -0500

    Exercise 1.1 completed

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,5 +1,34 @@
+const Header = (props) =>{
+  return (
+    <>
+    <h1>{props.course}</h1>
+    </>
+  )
+}
 
-function App() {
+const Content = (props) =>{
+  return (
+    <>
+    <p>{props.parts[0]} {props.exercises[0]}</p>
+    <p>{props.parts[1]} {props.exercises[1]}</p>
+    <p>{props.parts[2]} {props.exercises[2]}</p>
+    </>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+}
+
+
+
+
+
+
+
+const App = () => {
  const course = 'Half Stack application development'
  const part1 = 'Fundamentals of React'
  const exercises1 = 10
@@ -11,17 +40,9 @@ function App() {
   return (
     <>
       <div>
-        <h1>{course}</h1>
-        <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+        <Header course={course} />
+        <Content parts={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
+        <Total total={exercises1 + exercises2 + exercises3}/>
       </div>
     </>
   )
