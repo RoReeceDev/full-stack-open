03d8f04fe4c244c1f0ed675bc2e817ada5f751c8
RoReeceDev <roreecedev@gmail.com>
2024-02-08 20:19:19 +0200

    Refactoring Components code along

--- part1/part1-project/src/App.jsx
+++ part1/part1-project/src/App.jsx
@@ -1,22 +1,83 @@
+import { useState } from 'react'
 
-const Hello = (props) => {
-  console.log(props)
-  return(
+
+// const Hello = (props) => {
+//   // console.log(props)
+
+//   const bornYear = () => {
+//     const yearNow = new Date().getFullYear()
+//     return yearNow - props.age
+//   }
+
+//   return (
+//     <div>
+//       <p>
+//         Hello {props.name}, you are {props.age} years old
+//       </p>
+//       <p>
+//         So you were probably born in {bornYear()}
+//       </p>
+//     </div>
+//   )
+// }
+
+ 
+
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>{text}</button>
+  )
+
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
 const App = () => {
-  const friends = [
-    'Peter',
-    'Maya'
-  ]
+
+  const [ left, setLeft ] = useState(0)
+  const [ right, setRight ] = useState(0)
+  const [ allClicks, setAllClicks ] = useState([])
+  const [ total, setTotal ] = useState(0)
+  
+
+  const handleLeftClick = () => {
+    setAllClicks(allClicks.concat('L'))
+    console.log('left before', left)
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    console.log('left after', left)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAllClicks(allClicks.concat('R'))
+    console.log('right before', right)
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    console.log('right after', right)
+    setTotal(left + updatedRight )
+  }
+
   return (
     <>
       <div>
-        <p>{friends}</p>
+        {left}
+        <Button handleClick={handleLeftClick} text="left" />
+        <Button handleClick={handleRightClick} text="right" />
+        {right}
+        <History allClicks={allClicks} />
+        <p>total : {total}</p>
       </div>
     </>
   )
