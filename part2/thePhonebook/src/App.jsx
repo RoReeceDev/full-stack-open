1bb521d6b83ae0018540244921cbdceac29d4a9f
RoReeceDev <roreecedev@gmail.com>
2024-03-01 12:31:40 -0500

    Exercise 2.9 completed

--- part2/thePhonebook/src/App.jsx
+++ part2/thePhonebook/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 
 function App() {
-  const [persons, setPersons] = useState([{
-    name: 'Arto Hellas',
-    phone: '040-123456'
-  }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
+  ])
 
   const [newName, setNewName] = useState('')
-  
+
   const [newPhone, setNewPhone] = useState('')
+
+  const [ searchContact, setSearchContact ] = useState('')
 
   const handleNameChange = (e) => {
     console.log(e.target.value)
@@ -26,17 +30,18 @@ function App() {
     const nameObj = {
       name: newName,
       phone: newPhone,
+      id: persons.length + 1
     }
 
     const dulplicate = (n) => {
       for (let i = 0; i < persons.length; i++) {
         if (JSON.stringify(n) === JSON.stringify(persons[i])) {
           return true
-        } 
+        }
       }
     }
 
-    if(dulplicate(nameObj)){
+    if (dulplicate(nameObj)) {
       alert(`${nameObj.name} is already added to the phonebook`)
     } else if (!dulplicate(nameObj)) {
       setPersons(persons.concat(nameObj))
@@ -45,11 +50,28 @@ function App() {
     setNewName('')
   }
 
+  const handleSearch = (e) => { 
+    console.log(e.target.value)
+    setSearchContact(e.target.value)
+  }
+ 
+
+  const contactsToShow = searchContact ? persons.filter(person => person.name.toLowerCase().includes(searchContact.toLowerCase()))
+  : persons
+
   console.log(persons)
 
   return (
     <>
       <h2>Phonebook</h2>
+      <div>
+        <input 
+        type="text"
+        value={searchContact}
+        onChange={handleSearch}
+        />
+      </div>
+      <h3>Add a New Contact</h3>
       <form onSubmit={addContact}>
         <div>
           name: <input
@@ -58,18 +80,18 @@ function App() {
           />
         </div>
         <div>
-        phone: <input
-          value={newPhone}
-          onChange={handlePhoneChange}
+          phone: <input
+            value={newPhone}
+            onChange={handlePhoneChange}
           />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name} {person.phone}</li>)}
+        {contactsToShow.map(person => <li key={person.id}>{person.name} {person.phone}</li>)}
       </ul>
     </>
   )
